d60c7e8269cccab748b13f7d447efd510d362270
유형우/애버커스 <db6194@iabacus.co.kr>
2024-03-07 17:45:52 +0900

    [fix] : controller 추가

--- src/notion/notion.controller.ts
+++ src/notion/notion.controller.ts
@@ -1,18 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { NotionService } from './notion.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Observable } from 'rxjs';
+import { NotionResDto } from './notion.resDto';
 
 @Controller('notion')
 @ApiTags('Notion API')
 export class NotionController {
   constructor(private notionService: NotionService) {}
 
-  @Get('list')
+  @Get('list/:pageId')
   @ApiOperation({
     summary: 'Notion List 조회',
     description: 'Notion의 폴더안에 List 조회하는 API',
   })
-  get() {
-    return this.notionService.getBlock('8ea8a266-b87e-47bd-be61-aa0da70f15bc');
+  get(@Param('pageId') pageId: string): Observable<NotionResDto> {
+    return this.notionService.getBlock(pageId);
   }
 }
